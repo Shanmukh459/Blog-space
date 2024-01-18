088462e802f8a9c7f19164cc486c33fc22258d19
Shanmukh459 <saikrrish459@gmail.com>
2024-01-18 13:38:49 -0800

    form reset code

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
+let postsArray = []
+const titleInput = document.getElementById("post-title")
+const bodyInput = document.getElementById("post-body")
+const inputForm = document.getElementById("input-form")
+
+function renderPosts() {
+    const postsHtml = postsArray.map(post => {
+        return `
+                <div class="post">
+                    <h2>${post.title}</h2>
+                    <p>${post.body}</p>
+                </div>`
+    }).join('')
+    document.getElementById("posts-container").innerHTML = postsHtml
+}
+
+// function clearForm() {
+//     titleInput.value = ""
+//     bodyInput.value = ""
+// }
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        const postsHtml = postsArr.map(post => {
-            return `
-                    <div class="post">
-                        <h2>${post.title}</h2>
-                        <p>${post.body}</p>
-                    </div>`
-        }).join('')
-    document.getElementById("posts-container").innerHTML = postsHtml
+        postsArray = data.slice(0, 5)
+        renderPosts()        
 })
 
-document.querySelector("form").addEventListener("submit", function(e) {
+inputForm.addEventListener("submit", function(e) {
     e.preventDefault()
-    const postTitle = document.getElementById("post-title").value
-    const postBody = document.getElementById("post-body").value
+    const postTitle = titleInput.value
+    const postBody = bodyInput.value
     const data = {
         title: postTitle,
         body: postBody,
     }
 
-    fetch("https://apis.scrimba.com/jsonplaceholder/posts",
-    {
+    const options = {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
             "Content-Type": "application/json"
         }
-    })
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
         .then(res => res.json())
-        .then(data => {
-            document.getElementById("posts-container").innerHTML += `
-                <div class="post">
-                <h2>${data.title}</h2>
-                <p>${data.body}</p>
-            </div>`   
+        .then(post => {
+            postsArray.unshift(post)
+            renderPosts()  
+            // clearForm()
+            inputForm.reset()
         })
 })
 
